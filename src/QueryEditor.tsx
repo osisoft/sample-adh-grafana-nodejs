f5918992e5e114e304e4b8c0f7658afc82a72359
Collin Bardini <cbardini@osisoft.com>
2022-05-12 15:26:46 -0700

    Update QueryEditor.tsx

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,6 +1,6 @@
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { AsyncSelect, InlineFormLabel } from '@grafana/ui';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Debounce } from './debounce';
 
 import { SdsDataSource } from './DataSource';
@@ -8,36 +8,32 @@ import { SdsDataSourceOptions, SdsQuery } from './types';
 
 type Props = QueryEditorProps<SdsDataSource, SdsQuery, SdsDataSourceOptions>;
 
-export class QueryEditor extends PureComponent<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
+export const QueryEditor = ({ query, datasource, onChange, onRunQuery }: Props) => {
+  query = { ...query };
 
-  onSelectedStream = (value: SelectableValue<string>) => {
-    const { onChange, query } = this.props;
+  const onSelectedStream = (value: SelectableValue<string>) => {
     onChange({ ...query, streamId: value.value || '', streamName: value.label || '' });
   };
 
-  render() {
-    const query = this.props.query;
-    const selectStream: SelectableValue<string> = { label: query.streamName, value: query.streamId };
+  const debouncedGetStreams = Debounce((inputvalue: string) => {
+    return datasource.getStreams(inputvalue);
+  }, 1000);
 
-    return (
-      <div className="gf-form">
-        <InlineFormLabel width={8}>Stream</InlineFormLabel>
-        <AsyncSelect
-          defaultOptions={true}
-          width={50}
-          loadOptions={Debounce((inputvalue: string) => {
-            return this.props.datasource.getStreams(inputvalue);
-          }, 1000)}
-          value={selectStream}
-          onChange={(inputvalue) => this.onSelectedStream(inputvalue)}
-          placeholder="Select Stream"
-          loadingMessage={'Loading streams...'}
-          noOptionsMessage={'No streams found'}
-        />
-      </div>
-    );
-  }
-}
+  const selectStream: SelectableValue<string> = { label: query.streamName, value: query.streamId };
+
+  return (
+    <div className="gf-form">
+      <InlineFormLabel width={8}>Stream</InlineFormLabel>
+      <AsyncSelect
+        defaultOptions={true}
+        width={50}
+        loadOptions={(inputvalue) => debouncedGetStreams(inputvalue)}
+        value={selectStream}
+        onChange={(inputvalue) => onSelectedStream(inputvalue)}
+        placeholder="Select Stream"
+        loadingMessage={'Loading streams...'}
+        noOptionsMessage={'No streams found'}
+      />
+    </div>
+  );
+};
